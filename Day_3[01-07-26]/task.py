cc7585c550902d8f436c04aaccfdc65eaaae0d49
sarikadev30 <sarika.ss.net@gmail.com>
2026-07-01 22:05:55 +0530

    Added day 3

---
+++ Day_3[01-07-26]/task.py
@@ -0,0 +1,210 @@
+# Type Casting
+
+# Implicit Type Casting
+
+# x=90
+# print(type(x))
+# x="Sam"
+# print(type(x))
+
+# Explicit Type Casting
+
+# float()
+# y=45
+# print(y, type(y))
+# y=float(y)
+# print(y, type(y))
+# z="56"
+# print(z,type(z))
+# z=float(z)
+# print(z,type(z))
+
+# int()
+# a=890.67
+# print(a,type(a))
+# a=int(a)
+# print(a,type(a))
+# b="32"
+# print(b,type(b))
+# b=int(b)
+# print(b,type(b))
+
+# str()
+# a=9811206344
+# print(a,type(a))
+# a=str(a)
+# print(a,type(a))
+
+# bool()
+# a=0
+# print(a,type(a))
+# a=bool(a)
+# print(a,type(a))
+# b="Sam"
+# print(b,type(b))
+# b=bool(b)
+# print(b,type(b))
+# ...........................................................................
+
+# Multiple values to multiple variables
+# x=89
+# y=90
+# z=7
+# print(x,y,z)
+
+# x,y,z = 89,90,7
+# print(x,y,z)
+# .............................................................................
+
+# One value to multiple variables
+# a=90
+# b=90
+# c=90
+# print(a,b,c)
+
+# a=b=c=90
+# print(a,b,c)
+
+# ..............................................................................
+# Mathematical Operators
+
+# Addition Operator (+)
+# print(3+7)
+# a=90
+# b=78
+# print(a+b)
+
+# Subtraction Operator  (-)
+# print(8-5)
+
+# Multiplication Operator (*)
+# print(2*6)
+
+# Division Operator (/)  (float)
+# print(8/4)
+
+# Modulo Operator => gives the remainder (%)
+# print(9%3)
+
+# Exponentiation Operator => compute the power of any number  (**)     (a**2 => a*a)
+# print(2**5)
+
+# ..................................................................................
+# Problem 1
+# Maths => 98
+# Science => 56
+# Social Science => 90
+# Print Total Marks
+# Print Average Marks
+
+# .................................................................................
+# Problem 2
+# d = 3
+# e = 2
+# f = 4
+# sum = (d * 2) + (e * 2) + (f * 2)
+# sum = (d+e+f)*2
+# print(sum)
+# ..................................................................................
+# String Concatination
+
+# x=9
+# y=7.9
+# print(x+y)
+
+# x="Hello"
+# y="Vrishay"
+# z="How r u ?"
+# print(x+y+z)
+
+# .....................................................................................
+
+# Comparison Operators
+# > (Greater Than)
+# print(2>7)
+
+# >= (Greater Than Equal To)
+# print(7>=7)
+
+# < (Less Than)
+# print(5<7)
+# <= (Less Than Equal To)
+# print(5<=7)
+
+# == (Equal)
+# print(5==7)
+
+# != (not Equal)
+# print(5!=7)
+
+# ................................................................................
+# Problem 1
+# ram = "ram"
+# print(5 == 5)              # T
+# print("dam" == "dam")      # T
+# print(5 == "5")            # F
+# print(ram == "ram")        # T
+# print(5 == "ram")          # F
+# print(5 != 5)              # F
+# print(6 == "6")            # F
+# print(6 != "6")            # T
+# print(6 != 7)              # T
+# ..............................................
+# Problem 2
+# fan = 5
+# a = "fan"
+# b = fan
+# print(a == b)    # F
+# print(a != b)    # T
+# .............................................
+# Problem 3:
+# age_of_ram = 25; age_of_mohan = 30;
+# Check whether the Ram age is greater than Mohan or not.
+
+# age_of_ram = 25
+# age_of_mohan = 30
+# print(age_of_ram>age_of_mohan)
+# x=age_of_ram>age_of_mohan
+# print(x)
+# ...........................................................................
+# HomeWOrk Assignment 
+# ...............................................................................
+# Problem 1:
+# sunil_marks = 36; passing_marks = 35;
+# Check whether Sunil is passed or not, where the passing mark is 35
+# sunil_marks = 33
+# passing_marks = 33
+# ..................................
+# Problem 2:
+# sunil_marks = 34; passing_marks = 35;
+# Check whether Sunil failed or not, where the passing mark is 35
+# sunil_marks = 34
+# passing_marks = 35
+# ..................................
+# Problem 3:
+# Those customers will be eligible for the amazon discount
+# whose spending is equal to or above 4000.
+# Check whether a customer Sam is eligible for a discount or not
+# Sam_Spendings = 4500
+
+
+# ........................................................................
+# Problem 4:
+# Check whether Danny is eligible for driving a vehicle in India => 18
+# Danny_age = 19
+
+# ...........................................................................
+
+# Problem 5:
+# a = 5
+# b = 6
+# c = "6"
+# d = -2
+# e = "m"
+# print(b == c)  
+# print(c == e)  
+# print(a >= c)  
+# print(d <= a)  
+# print(b != c)
+# print(3.0 == 3) 
+# .......................................................................................
