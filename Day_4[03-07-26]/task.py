c3c117ebfd68316b505d4c2e75e44a3f06d83c76
sarikadev30 <sarika.ss.net@gmail.com>
2026-07-04 14:54:07 +0530

    added day-4

---
+++ Day_4[03-07-26]/task.py
@@ -0,0 +1,159 @@
+# Practice Problems 
+# Check if the Number is Positive.
+# x=-89
+# print(x>0)
+# ...........................................
+# Check if the number is even 
+# x=88
+# print(x%2==0)
+# ..........................................
+# Check if the number is odd
+# x=89
+# print(x%2==1)
+# print(x%2!=0)
+# ...........................................
+# Check if the number is divisible by 5
+# x=94
+# print(x%5==0)
+# ...................................................................
+# Logical operators 
+
+# AND  => all needs to true
+# print(True and True and True)
+# print(False and True)
+# print(True and False)
+# print(False and False and True)
+
+# a = True
+# b = False
+# print(a and b)
+
+# p = 3 > 5
+# q = 9 >= 8
+# print(p and q) 
+# ................................................
+# OR => any one value needs to be true
+# print(True or True)
+# print(False or True)
+# print(True or False)
+# print(False or False)
+# print(False or False or True)
+# .................................................
+# NOT => Opposite of the expression
+
+# print(not False)
+# print(not True)
+# ........................................................................
+# Problem :
+# a = 9 < 11  #T
+# b = 2 == 3   # F
+# c = 10 > 3  # T
+# result = a and (b or c)
+#         # T and  T
+# print(result)
+
+# ...................................................................
+# x = 4 <= 4    # T
+# y = 10 < 5    # F
+# z = not (x and y)
+# print(z)
+# .................................................................
+
+# m = 0 != 0    # F
+# n = 1 == 1    # T
+# o = m and not n
+# print(o)
+# .................................................................
+
+# p = 6 == 6   #T
+# q = 5 != 5   #F
+# r = not (p or q)
+# print(r)
+
+# .................................................................
+
+# p = 10 < 5   # F
+# q = 3 == 3   # T
+# r = 7 != 7   # F
+# result = not (p or q and r)
+# print(result)
+
+# .............................................................................
+# Assignment Operators
+x=3
+# x=x+2
+# Add and assign 
+# x+=2
+# Subtract and assign
+# x-=2
+# Multiply and assign
+# x*=2
+# divide and assign
+# x/=2
+# modulo and assign
+# x%=2
+# exponent and assign
+# x**=2
+# floor division and assign
+# x//=2
+# print(x)
+
+# print(919//3)
+# ................................................................
+# Problems
+# x = 8
+# x *= 2
+# x -= 4
+# print(x)
+# ...................................................
+# a = 5
+# b = 3
+# a %= b
+# print(a)
+# ...................................................
+# x = 101
+# x -= 25
+# x //= 5
+# print(x)
+# .................................................
+# b = 10
+# b %= 4
+# b *= 2
+# print(b)
+# .............................................................................
+# Membership Operators
+# in 
+#  not in
+
+# x="How are you ?"
+# y="Are"
+# print(y in x)
+# print(y not in x)
+# List 
+# x=[56,78,89,9,34,88]
+# print(56 in x)
+# ....................................................................................
+# greeting = "good morning"
+# print("good" in greeting)
+# print("Good" in greeting)
+# print("night" not in greeting)
+# ....................................................................................
+# Swap two numbers
+
+# a=90
+# b=67
+# print(a, b)
+# taking a temp variable 
+# c=a   # 90
+# a=b    # 67 
+# b=c    # 90
+# print(a, b)
+# ....................................
+# a=a+b          # a => 67+90 => 157
+# b=a-b           # b => 157-67 => 90
+# a=a-b             # a => 157 - 90  => 67
+# print(a,b)
+# ...........................................
+# a,b=b,a
+# print(a,b)
+# .....................................................................................
