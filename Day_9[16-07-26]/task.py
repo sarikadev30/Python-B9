6a0762726f254dfb7a9daaa4d516be8e4b5e8d98
sarikadev30 <sarika.ss.net@gmail.com>
2026-07-16 22:08:54 +0530

    added day - 9

---
+++ Day_9[16-07-26]/task.py
@@ -0,0 +1,92 @@
+# Multiplication table 
+
+# n=5
+# i=1
+
+# while i<=10:
+#     print(f"{n} * {i} = {n*i}")
+#     i+=1
+
+# ................................................
+
+# WHILE WITH BREAK
+
+# i=1
+# while i<5:
+#     print(i)
+#     if i==2:
+#         break
+#     i+=1
+# print("End")
+
+# DRY RUN
+# i=1    1<5 => T   1 1==2=>F  i=1+1=2
+# i=2    2<5 => T   2 2==2=>T break
+# "End"
+
+# ...................................................
+# i = 1
+# print("Start")
+# while i < 5:
+#     if i == 3:
+#         break
+#     print(i)
+#     i += 1
+# print("End")
+
+# .....................................................
+#  While With Continue
+# continue => skips the execution
+
+# i = 1
+# print("Start")
+# while i < 5:
+#     i += 1
+    
+#     if i == 3:
+#         continue
+#     print(i)
+    
+
+# print("End")
+# .........................................................................
+# #  Find the First Multiple of 7 (1:10)    (using break)
+
+# ....................................................................................
+# FOR LOOP
+
+# for i in sequence:
+#     print(i)
+
+
+# range(start , end, step)
+# default value of start = 0
+# default value of step = 1
+
+
+# for i in range(0,5,2):
+#     print(i)
+
+# for i in range(5):
+#     print(i)
+
+# for i in range(2,5):
+#     print(i)
+
+# for i in range(5,0,-1):
+#     print(i)
+
+
+# for i in range(5,-1,-1):
+#     print(i)
+
+# ...............................................................................
+# s=0
+# for i in range(1,101):
+#     s+=i
+
+# print(s)
+
+
+
+
